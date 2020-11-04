ede07ff89beff45d2bfcca78ffa986ccf8a3097e
whatplace <Christopher.place99@gmail.com>
2020-11-04 16:45:52 -0500

    formatted files and updated html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-const { app, BrowserWindow, ipcRenderer } = require('electron');
-const path = require('path');
+//
+//	Christopher Place
+//	CS498 Capstone
+//  Filename: index.js
+//
+//  Based on Electron Forge Template
+//
 
-
+const { app, BrowserWindow, ipcRenderer } = require("electron");
+const path = require("path");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
   app.quit();
 }
 
@@ -13,26 +20,18 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 1300,
     height: 800,
-    backgroundColor: '#9E9E9E',
-    webPreferences:{
+    backgroundColor: "#9E9E9E",
+    webPreferences: {
       nodeIntegration: true,
-      worldSafeExecuteJavaScript: true
-    }
-    
+      worldSafeExecuteJavaScript: true,
+    },
   });
 
-
-
-
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools();
-
-
-
-
 };
 
 /*
@@ -60,22 +59,21 @@ function getPlugins() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
--- src/interpreter.js
+++ src/interpreter.js
@@ -1,111 +1,101 @@
+//
+//	Christopher Place
+//	CS498 Capstone
+//  Filename: interpreter.js
+//
+//  Utilized for obtaining results from vol.py
+//
 
+const $ = require("jquery");
+const { stdout } = require("process");
+const dt = require("datatables.net")();
+const dtbs = require("datatables.net-bs4")(window, $);
 
-const $ = require('jquery');
-const { stdout } = require('process');
-const dt = require('datatables.net')();
-const dtbs = require('datatables.net-bs4')(window, $);
-
-require( 'jszip' );
-require( 'pdfmake' );
-require( 'datatables.net-bs4' )();
-require( 'datatables.net-buttons-bs4' )();
-require( 'datatables.net-buttons/js/buttons.html5.js' )();
-require( 'datatables.net-colreorder-bs4' )();
-require( 'datatables.net-responsive-bs4' )();
-
+require("jszip");
+require("pdfmake");
+require("datatables.net-bs4")();
+require("datatables.net-buttons-bs4")();
+require("datatables.net-buttons/js/buttons.html5.js")();
+require("datatables.net-colreorder-bs4")();
+require("datatables.net-responsive-bs4")();
 
 function getData() {
-  const path = require('path');
-  let {PythonShell} = require('python-shell');
-    
-    if(typeof document.getElementsByTagName('input')[0].files[0] === 'undefined'){
-      console.log('Error file not defined');
-      //document.getElementById("pythonError").textContent = 'Hello World';
-      document.getElementById('pythonError').innerHTML = 'Error file not defined';
-      $('#pythonError').show();
-      
-    }
-    else{
-      filePath = document.getElementsByTagName('input')[0].files[0].path;
-      command = document.getElementById('command').value
-      console.log(command)
-        var options = {
-            //Change script path before exporting with npm make
-              scriptPath : path.join('src','volatility3'),
-            //scriptPath : path.join('resources','app','src','volatility3'),
-            pythonOptions: ['-u'],
-            args : ['-qrjson','-f',filePath, command],
-            pythonPath: 'C:\\Python38\\python.exe',
-            //args : ['-qrjson','frameworkinfo.FrameworkInfo'],
+  const path = require("path");
+  let { PythonShell } = require("python-shell");
 
-        };
-        
-        let pyshell = new PythonShell('vol.py', options);
+  if (
+    typeof document.getElementsByTagName("input")[0].files[0] === "undefined"
+  ) {
+    console.log("Error file not defined");
+    //document.getElementById("pythonError").textContent = 'Hello World';
+    document.getElementById("pythonError").innerHTML = "<strong>Error:</strong> File not defined.";
+    $("#pythonError").show();
+  } else {
+    filePath = document.getElementsByTagName("input")[0].files[0].path;
+    command = document.getElementById("command").value;
+    console.log(command);
+    var options = {
+      //Change script path before exporting with npm make
+      scriptPath: path.join("src", "volatility3"),
+      //scriptPath : path.join('resources','app','src','volatility3'),
+      pythonOptions: ["-u"],
+      args: ["-qrjson", "-f", filePath, command],
+      pythonPath: "C:\\Python38\\python.exe",
+      //args : ['-qrjson','frameworkinfo.FrameworkInfo'],
+    };
 
-        
-        
-        var logs ='';
-        pyshell.on('message', function(message) {
-          logs = logs + message;
-        });
-      
-        pyshell.on('error', function (err) {
-          console.log(' error ', err);
-          document.getElementById('pythonError').innerHTML = 'Error running command';
-          $('#pythonError').show();
-        });
-      
-        pyshell.end(function (code,signal) {
-          if(code = 1){
-              let data = ''
-              if(logs=='') {
-              }
-              else{
-              data = JSON.parse(logs+']')
-              console.log(data)
-              
-              // generate DataTables columns dynamically
-              let columns = [];
-              Object.keys(data[0]).forEach( key => columns.push({ title: key, data: key }) )
-              console.log(columns)
-              // Create DataTable
-              
-              if ( $.fn.DataTable.isDataTable('#output') ) {
-                console.log("Destroying DT");
-                $('#output').DataTable().clear().destroy();
-                $('#output').empty();
-                console.log("Table Status: "+ $.fn.DataTable.isDataTable('#output'));
-              }
-              
+    let pyshell = new PythonShell("vol.py", options);
 
-             
-              $('#output').DataTable({ 
-                  dom: 'Bfrtip',
-                  data: data,
-                  columns: columns,
-                  processing: true,
-                  "search": {
-                    "regex": true,
-                    "smart": false
-                  },
-                  buttons: [
-                    'csv', 'excel', 'pdf'
-                    ],
-                    
-              });
-            }
-              
-              
+    var logs = "";
+    pyshell.on("message", function (message) {
+      logs = logs + message;
+    });
+
+    pyshell.on("error", function (err) {
+      console.log(" error ", err);
+      document.getElementById("pythonError").innerHTML =
+        "<strong>Error:</strong> Volatility encountered a problem running command.";
+      $("#pythonError").show();
+    });
+
+    pyshell.end(function (code, signal) {
+      if ((code = 1)) {
+        let data = "";
+        if (logs == "") {
+        } else {
+          data = JSON.parse(logs + "]");
+          console.log(data);
+
+          // generate DataTables columns dynamically
+          let columns = [];
+          Object.keys(data[0]).forEach((key) =>
+            columns.push({ title: key, data: key })
+          );
+          console.log(columns);
+          // Create DataTable
+
+          if ($.fn.DataTable.isDataTable("#output")) {
+            console.log("Destroying DT");
+            $("#output").DataTable().clear().destroy();
+            $("#output").empty();
+            console.log(
+              "Table Status: " + $.fn.DataTable.isDataTable("#output")
+            );
           }
-        });
-        
+
+          $("#output").DataTable({
+            dom: "Bfrtip",
+            data: data,
+            columns: columns,
+            processing: true,
+            search: {
+              regex: true,
+              smart: false,
+            },
+            buttons: ["csv", "excel", "pdf"],
+          });
+        }
       }
-      
-    
+    });
+  }
 }
-
-
-
-
-
-
